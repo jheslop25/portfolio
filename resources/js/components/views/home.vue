e665860c38a46d001bc04aaa058ab19be6eeff1e
John Heslop <johnheslop24@gmail.com>
2020-05-15 04:04:33 +0000

    managed to get an animation working at last. sigh.

--- resources/js/components/views/home.vue
+++ resources/js/components/views/home.vue
@@ -1,19 +1,28 @@
 <template>
   <v-row>
-      <v-col>
-          <v-card>
-              <v-card-title>hello world</v-card-title>
-          </v-card>
-      </v-col>
+    <v-col>
+      <v-card>
+        <v-card-title>hello world</v-card-title>
+      </v-card>
+    </v-col>
   </v-row>
 </template>
 
 <script>
+//import particlesJS from "particles.js";
+
 export default {
-    name: 'home'
-}
+  name: "home",
+  methods: {},
+  data() {
+    return {};
+  },
+  mounted() {
+    particlesJS.load("particles-js", 'js/particles.json', function() {
+      console.log("callback - particles-js config loaded");
+    });
+  }
+};
 </script>
-
-<style>
-
+<style scoped>
 </style>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,19 +1,25 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>John Heslop - Web Developer</title>
-        <script src="{{ asset('js/app.js') }}" defer></script>
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-        <!-- Styles -->
-    </head>
-    <body>
-       <div id="app">
-            <app></app>
-       </div>
-    </body>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>John Heslop - Web Developer</title>
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <!-- Styles -->
+</head>
+
+<body>
+    <div id="app">
+        <app></app>
+    </div>
+    <script src="http://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script> <!-- stats.js lib -->
+    <script src="http://threejs.org/examples/js/libs/stats.min.js"></script>
+</body>
+
 </html>
